830577b1a3a16f009640cccd119f8fc18031a82f
Thewads <rosswaddell1990@gmail.com>
2023-02-08 23:15:36 +0000

    Adding in quick hacked together console app for helping generate the paint files Adding in proacryl.json for the basic paint range

---
+++ MiniPaintHelper/RgbToLabHelper/Program.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Services.Models;
+
+Console.WriteLine("Tool for helping generate paint files");
+Console.WriteLine("Give a path to a paint file with RGB populated to automatically generate the Lab values");
+
+var filePath = Console.ReadLine();
+
+if (filePath != null)
+{
+    var fileData = File.ReadAllText(filePath);
+
+    var paints = JsonConvert.DeserializeObject<List<Paint>>(fileData);
+
+    if (paints != null)
+        foreach (var paint in paints)
+        {
+            var rgbColor = new ColorMine.ColorSpaces.Rgb { R = paint.Rgb.R, G = paint.Rgb.G, B = paint.Rgb.B };
+            var labColor = rgbColor.To<ColorMine.ColorSpaces.Lab>();
+
+            paint.Lab.L = Math.Round(labColor.L, 2);
+            paint.Lab.A = Math.Round(labColor.A, 2);
+            paint.Lab.B = Math.Round(labColor.B, 2);
+        }
+    
+    File.WriteAllText(filePath, JsonConvert.SerializeObject(paints, Formatting.Indented));
+}
