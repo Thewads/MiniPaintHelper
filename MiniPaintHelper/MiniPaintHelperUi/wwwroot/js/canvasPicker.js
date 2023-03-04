b3279394471e2cf24241a679a7c297ee77fda9e8
Thewads <rosswaddell1990@gmail.com>
2023-03-04 14:16:09 +0000

    Updating file upload to now be more blazorfied Now supports drag/drop file upload, as well as copy/paste

---
+++ MiniPaintHelper/MiniPaintHelperUi/wwwroot/js/canvasPicker.js
@@ -0,0 +1,34 @@
+﻿export function initializeCanvasPicker(canvas, paintFromImageRef) {
+    function onClick(e) {
+        const canvas = $('#canvas_picker')[0];
+        const context = canvas.getContext('2d');
+
+        let x = event.pageX - $(this).offset().left;
+        let y = event.pageY - $(this).offset().top;
+        let img_data = context.getImageData(x,y , 1, 1).data;
+        let R = img_data[0];
+        let G = img_data[1];
+        let B = img_data[2];
+        paintFromImageRef.invokeMethodAsync("ColorPicked", R, G, B);
+    }
+    
+    canvas.addEventListener('click', onClick);
+
+    return {
+        dispose: () => {
+            canvas.removeEventListener('click', onClick)
+        }
+    }
+}
+export function setCanvasImage(base64) {
+    const img = new Image();
+    img.onload = function(){
+        const canvas = $('#canvas_picker')[0];
+        const context = canvas.getContext('2d');
+        canvas.width  = this.width;
+        canvas.height = this.height;
+        context.drawImage(this, 0, 0);
+    };
+    img.src = 'data:image/png;base64,' + base64;
+}
+
---
+++ MiniPaintHelper/MiniPaintHelperUi/wwwroot/js/filePaste.js
@@ -0,0 +1,37 @@
+﻿export function initializeFilePaste() {
+    function imageIsLoaded(e) {
+        const img = new Image();
+        img.onload = function(){
+            const canvas = $('#canvas_picker')[0];
+            const context = canvas.getContext('2d');
+            canvas.width  = this.width;
+            canvas.height = this.height;
+            context.drawImage(this, 0, 0);
+        };
+        img.src = e.target.result;
+    }
+    function onPaste(event) {
+        // use event.originalEvent.clipboard for newer chrome versions
+        let items = (event.clipboardData  || event.originalEvent.clipboardData).items;
+        let blob = null;
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].type.indexOf("image") === 0) {
+                blob = items[i].getAsFile();
+            }
+        }
+        if (blob !== null) {
+            let reader = new FileReader();
+            reader.onload = imageIsLoaded
+            reader.readAsDataURL(blob);
+        }
+    }
+    
+    document.addEventListener('paste', onPaste);
+
+    
+    return {
+        dispose: () => {
+           document.removeEventListener('paste', onPaste);
+        }
+    }
+}
